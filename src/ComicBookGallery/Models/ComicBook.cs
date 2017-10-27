6e04313733524b1ac894a3f1390e19d1ba26474e
Jay Watson <jaywatson2.0@outlook.com>
2017-10-27 05:30:43 -0500

    Updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,11 +8,27 @@ namespace ComicBookGallery.Models
 	public class ComicBook
 	{
 		public int Id { get; set; }
-		public string Series { get; set; }
+		public string SeriesTitle { get; set; }
 		public int IssueNumber { get; set; }
 		public string DescriptionHtml { get; set; }
 		public Artist[] Artists { get; set; }
 		public bool Favorite { get; set; }
 
+		public string DisplayText
+		{
+			get
+			{
+				return SeriesTitle + " #" + IssueNumber;
+			}
+			// do not define setter. cannot be changed 
+		}
+
+		public string CoverImageFileName
+		{
+			get
+			{
+				return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+			}
+		}
 	}
 }
