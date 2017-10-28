6d639d24b242ec45209616fd9e535059068cb259
Jay Watson <jaywatson2.0@outlook.com>
2017-10-28 05:38:21 -0500

    Updated the Comic Book Detail view layout

--- src/ComicBookGallery/Controllers/TestTestController.cs
+++ src/ComicBookGallery/Controllers/TestTestController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,24 @@ namespace ComicBookGallery.Controllers
 {
 	public class TestTestController : Controller
 	{
+		public ActionResult AnotherPage()
+		{
+			var comicBook = new ComicBook()
+			{
+				SeriesTitle = "fred",
+				IssueNumber = 39843,
+				DescriptionHtml = "blah blah blah",
+				Artists = new Artist[]
+				{
+					new Artist() { Name = "jay", Role = "husband" },
+					new Artist() { Name = "Cass", Role = "Wife" }
+				}
+			};
+
+			return View(comicBook);
+		}
+		
+		
 		// GET: TestTest
 		public ActionResult Index()
 		{
@@ -18,5 +37,7 @@ namespace ComicBookGallery.Controllers
 		{
 			return "this is a test";
 		}
+
+
 	}
 }
