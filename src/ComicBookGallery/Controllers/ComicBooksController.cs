045708ffa2207d130ce722353f7732efda2c8046
Jay Watson <jaywatson2.0@outlook.com>
2017-10-29 10:50:08 -0500

    Updated comic books controller to use repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ComicBookGallery.Models;
+using ComicBookGallery.Data;
 
 namespace ComicBookGallery.Controllers
 {
 	public class ComicBooksController : Controller
 	{
+		private ComicBookRepository _comicBookRepository = null;
+
+		public ComicBooksController()
+		{
+			_comicBookRepository = new ComicBookRepository();
+		}
+
+
 		// GET: ComicBooks
 		public ActionResult Index()
 		{
@@ -22,23 +31,13 @@ namespace ComicBookGallery.Controllers
 			return View();
 		}
 
-		public ActionResult Detail()
+		public ActionResult Detail(int? id)
 		{
-			
-			var comicBook = new ComicBook()
+			if (id == null)
 			{
-				SeriesTitle = "The Amazing Spider-Man",
-				IssueNumber = 700,
-				DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-				Artists = new Artist[]
-				{
-					new Artist() {Name = "Dan Slott", Role = "Script"},
-					new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
-					new Artist() {Name = "Victor Olazaba", Role = "Inks"},
-					new Artist() {Name = "Edgar Delgado", Role = "Colors"},
-					new Artist() {Name = "Chris Eliopoulos", Role = "Letters"},
-				}
-			};
+				return HttpNotFound();
+			}
+			var comicBook = _comicBookRepository.GetComicBook((int)id);
 
 
 			return View(comicBook);
